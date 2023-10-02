0b05d00c0c45b41200aa5efb59768f8e5fa805ce
Egor <jdmjapanese69@gmail.com>
2023-10-02 21:22:34 +0300

    f
    f

---
+++ Assets/scripts/Button.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public GameObject objectToActivate;
+    private bool isActive = true;
+    void Start()
+    {
+        objectToActivate =  GameObject.FindWithTag("cube");
+    }
+
+    public void ToggleObjectActivation()
+    {
+        isActive = !isActive;
+        objectToActivate.SetActive(isActive);
+    }
+}
